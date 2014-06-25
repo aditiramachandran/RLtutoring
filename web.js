c921958bfe2b7a34e0a8d40ae4b5372024718684
Aditi Ramachandran <aditi@luminosity.wireless.yale.internal>
2014-06-25 14:19:59 -0400

    testing github

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World! Testing to see if this works.');
+  res.send('Hello World! Making sure github stuff works.');
 });
 
 var port = Number(process.env.PORT || 5000);
